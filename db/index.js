8d2078868aa6cfaa96fd32f05205dfd355326f39
Nnaemeka Joseph <nnaemekaxi@yahoo.com>
2022-11-09 12:14:40 +0100

    edited

--- db/index.js
+++ db/index.js
@@ -16,38 +16,4 @@ async function connect(){
     }
 }
 
-// const connect = async () => {
-//     try {
-//     mongoose.connect('mongod://localhost/toDoAppDb'); 
-//         console.log("Database Connected!")
-//     } catch (err) {
-//         console.log(err.message);
-
-//         //Exit with failure
-//         process.exit(1);
-//     }
-// }
-
 module.exports = connect;
-
-//create the connection function
-// const ConnectDb = () => {
-//     mongoose.connect(MONGO_URI, {
-//         userNewUrlParser: true,
-//         userCreateIndex: true,
-//         userUnifiedTopology: true,
-//         userFindAndModify:  false
-//     })
-//     .then(() => {
-//         console.log("Database Connected!");
-        
-//         //send data
-//     })
-//     .catch((err) => {
-//         console.log(err.message);
-
-//         //Exit with failure
-//         process.exit(1);
-//     })    
-// }
-
--- index.js
+++ index.js
@@ -1,13 +1,3 @@
-/**
- * 1. Create an express server
- * 2. Connect to MongoDb
- * 3. Initialize express
- * 4. Initialize express middleware
- * 5. Create a simple get request route (optional)
- * 6. Inject the routes
- * 7. Listen to the application
- */
-
 //create an express server
 const express = require("express");
 
@@ -27,10 +17,6 @@ const routes = require("./routes/toDoRoute");
 app.use(express.json({extended: false}));
 app.use("/", routes);
 
-//Create a simple get request route (optional)
-// app.get("/", (req, res) => {
-//     res.json({message: "Hello World!"})
-// })
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
